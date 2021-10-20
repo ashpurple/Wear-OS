ec22dc2304e04e62f94e42d6b9479575b134727f
parkchanbin54 <parks4754@gmail.com>
2021-10-20 23:07:31 +0900

    Step counter, Heartrate Sensor attached.

--- Wearable/src/main/java/com/example/android/wearable/watchface/watchface/BackService.java
+++ Wearable/src/main/java/com/example/android/wearable/watchface/watchface/BackService.java
@@ -24,7 +24,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class BackService extends Service implements RecognitionListener, SensorEventListener {
+public class BackService extends Service implements SensorEventListener {
     public Context context = this;
     public Handler handler = null;
     public Runnable runnable = null;
@@ -74,144 +74,6 @@ public class BackService extends Service implements RecognitionListener, SensorE
         return null;
     }
 
-    @Override
-    public void onBeginningOfSpeech() {
-        Log.e(LOG_TAG, "onBeginningOfSpeech");
-    }
-
-    @Override
-    public void onBufferReceived(byte[] buffer) {
-        Log.e(LOG_TAG, "onBufferReceived: " + buffer);
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-        Log.e(LOG_TAG, "onEndOfSpeech");
-    }
-
-    @Override
-    public void onError(int errorCode) {
-        String errorMessage = getErrorText(errorCode);
-        if (!errorMessage.equals("")){
-            Log.e(LOG_TAG, "FAILED " + errorMessage);
-        }
-    }
-
-    @Override
-    public void onEvent(int arg0, Bundle arg1) {
-        Log.e(LOG_TAG, "onEvent");
-    }
-
-    @Override
-    public void onPartialResults(Bundle arg0) {
-        Log.e(LOG_TAG, "onPartialResults");
-    }
-
-    @Override
-    public void onReadyForSpeech(Bundle arg0) {
-        Log.e(LOG_TAG, "onReadyForSpeech");
-    }
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onResults(Bundle results) {
-        try {
-            Log.e(LOG_TAG, "onResults");
-            ArrayList<String> matches = results
-                    .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            String text = "";
-            if (matches != null) {
-                for (String result : matches) {
-                    text = result;
-                    break;
-                }
-            }
-            Log.e("Result", text);
-
-            if (text.toLowerCase().trim().contains("call to")) {
-                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
-                String number = getPhoneNumber(text.toLowerCase().trim().replace("call to","").trim().toLowerCase().replace(".",""),context);
-                Log.e("SearchName","=>"+text.toLowerCase().trim().replace("call to","").trim().toLowerCase().replace(".",""));
-                Log.e("Number","=>"+number);
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setData(Uri.parse("tel:" + number));
-                context.startActivity(intent);
-            }
-            speech.stopListening();
-            handler.removeCallbacks(runnable);
-            handler.postDelayed(runnable, 1000);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onRmsChanged(float rmsdB) { }
-
-    public static String getErrorText(int errorCode) {
-        String message;
-        switch (errorCode) {
-            case SpeechRecognizer.ERROR_AUDIO:
-                message = "Audio recording error";
-                break;
-            case SpeechRecognizer.ERROR_CLIENT:
-                message = "Client side error";
-                break;
-            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-                message = "Insufficient permissions";
-                break;
-            case SpeechRecognizer.ERROR_NETWORK:
-                message = "Network error";
-                break;
-            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
-                message = "Network timeout";
-                break;
-            case SpeechRecognizer.ERROR_NO_MATCH:
-                message = "No match";
-                break;
-            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-                message = "RecognitionService busy";
-                break;
-            case SpeechRecognizer.ERROR_SERVER:
-                message = "error from server";
-                break;
-            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
-                message = "No speech input";
-                break;
-            default:
-                message = "Didn't understand, please try again.";
-                break;
-        }
-        return message;
-    }
-
-
-    @Override
-    public void onTaskRemoved(Intent rootIntent) {
-        if (speech != null) {
-            speech.destroy();
-            Log.e(LOG_TAG, "destroy");
-        }
-        super.onTaskRemoved(rootIntent);
-    }
-
-    public String getPhoneNumber(String name, Context context) {
-        String ret = null;
-        String selection = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" like'%" + name +"%'";
-        String[] projection = new String[] { ContactsContract.CommonDataKinds.Phone.NUMBER};
-        Cursor c = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                projection, selection, null, null);
-        if (c != null && c.moveToFirst()) {
-            ret = c.getString(0);
-        }
-        if (c != null) {
-            c.close();
-        }
-        if(ret==null)
-            ret = "Unsaved";
-        return ret;
-    }
 
     // SENSOR SHIT HERE
 
@@ -223,6 +85,10 @@ public class BackService extends Service implements RecognitionListener, SensorE
             // Start heart rate sensor
             final Sensor heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
             sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+            // Start step counter
+            final Sensor stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
             Log.d(SENSOR_TAG, "Sensor !");
         } else {
             Log.e(SENSOR_TAG, "SensorManager is null");
@@ -249,7 +115,11 @@ public class BackService extends Service implements RecognitionListener, SensorE
                     }
                 }
             }
-        }
+            else if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+                Log.e(SENSOR_TAG, "Received new step counter value: " + value);
+
+            }
+            }
         else {
             Log.e(SENSOR_TAG, " No heartrate Detected. Value: ");
         }
