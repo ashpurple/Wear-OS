f085ab59d48f88756fd33db7ea58d4a78bae5590
ashpurple <ashpurple9@gmail.com>
2021-10-24 12:36:40 +0900

    Update: GPS strucsture (Fail)

--- Wearable/src/main/java/com/example/android/wearable/watchface/watchface/BackService.java
+++ Wearable/src/main/java/com/example/android/wearable/watchface/watchface/BackService.java
@@ -23,8 +23,22 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+/* gps */
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.content.pm.PackageManager;
 
-public class BackService extends Service implements SensorEventListener {
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+
+public class BackService extends Service implements SensorEventListener, LocationListener {
     public Context context = this;
     public Handler handler = null;
     public Runnable runnable = null;
@@ -38,36 +52,47 @@ public class BackService extends Service implements SensorEventListener {
     private SensorManager sensorManager;
     public String SENSOR_TAG = "SensorEventListener";
     public Boolean hasCalledBecauseOfSensor = false;
+    //LOCATION MANAGER
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
         //Sensor Manager shit
-        sensorManager = getSystemService(SensorManager.class);
+        sensorManager = getSystemService(SensorManager.class); // sensor (heart rate, step)
+        locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE); // location (gps)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//        if (!hasGps()) {
+//            Log.e(LOCATION_TAG, "This hardware doesn't have GPS.");
+//            // Fall back to functionality that does not use location or
+//            // warn the user that location function is not available.
+//        }
         startSensors();
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(LOCATION_TAG, "Location Permission Fail");
+        } else{
+            Log.e(LOCATION_TAG, "Location Permission Success");
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+        }
+        getLocation();
         handler = new Handler();
         runnable = new Runnable() {
             public void run() {
-
                 handler.postDelayed(runnable, 5000);
             }
-
         };
-
         handler.postDelayed(runnable, 1000);
         return START_STICKY;
     }
 
     @Override
     public boolean stopService(Intent name) {
-        Log.e("Service", "Stop");
+        Log.e("Service", "Stop Service");
         speech.stopListening();
         sensorManager.unregisterListener(this);
         handler.removeCallbacks(runnable);
+        stopUsingGPS(); // gps
         return super.stopService(name);
     }
-
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -84,12 +109,12 @@ public class BackService extends Service implements SensorEventListener {
         if (sensorManager != null) {
             // Start heart rate sensor
             final Sensor heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
-            sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
             // Start step counter
             final Sensor stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
             sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
-            Log.d(SENSOR_TAG, "Sensor !");
+
         } else {
             Log.e(SENSOR_TAG, "SensorManager is null");
         }
@@ -97,37 +122,137 @@ public class BackService extends Service implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        Log.e(SENSOR_TAG, "onSensorChanged");
         // Check if a value is attached, if not we can ignore it
         if (sensorEvent.values.length > 0) {
             final float value = sensorEvent.values[0];
 
             if (sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE) {
-                Log.e(SENSOR_TAG, " heartrate Detected. Value: " + value);
-
-                if (value > 90){
+                Log.e(SENSOR_TAG, "heart Rate : " + value + "bpm");
+                getLocation();
+                if (value > 90) {
                     String text = "CRITICAL HEART RATE. Heart Rate: " + value;
-                    Log.d(SENSOR_TAG, text);
+                    Log.e(SENSOR_TAG, text);
                     Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
-
-                    if (hasCalledBecauseOfSensor == false){
-                       
-                    }
                 }
             }
-            else if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-                Log.e(SENSOR_TAG, "Received new step counter value: " + value);
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+                Log.e(SENSOR_TAG, "Step Count : " + value + "step");
 
             }
-            }
-        else {
-            Log.e(SENSOR_TAG, " No heartrate Detected. Value: ");
+        } else {
+
         }
+    }
 
+    /* gps location manager */
+    private boolean hasGps() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+    }
+
+
+    protected LocationManager locationManager;
+    Location location;
+    private double latitude;
+    private double longitude;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // meter
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 1 * 1; // milli * sec * min
+    public String LOCATION_TAG = "LocationManager";
+
+//    public BackService(Context context){ // constructor
+//        this.context = context;
+//        getLocation();
+//    }
+
+    public void printGPS() {
+        Log.e(LOCATION_TAG, "Latitude: " + latitude + " | Longitude: " + longitude);
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public Location getLocation() {
+        try {
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                Log.e(LOCATION_TAG, "Location can not be provided");
+            } else {
+                int hasFineLocationPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
+                if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Log.e(LOCATION_TAG, "Location Permission is not allowed");
+                    return null;
+                }
+//                if (isNetworkEnabled){
+//                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+//                    if (locationManager != null) {
+//                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+//                        if (location != null){
+//                            latitude = location.getLatitude();
+//                            longitude = location.getLongitude();
+//                            Log.e(LOCATION_TAG, "Location Updated by network");
+//                        } else {
+//                            Log.e(LOCATION_TAG, "Location  is null");
+//                        }
+//                    } else{
+//                        Log.e(LOCATION_TAG, "Location manager is null");
+//                    }
+//                }
+//                else{
+//                    Log.e(LOCATION_TAG, "Network is not connected");
+//                }
+                if (isGPSEnabled) {
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        } else {
+                            Log.e(LOCATION_TAG, "Location is null");
+                        }
+                    }
+                } else {
+                    Log.e(LOCATION_TAG, "Gps is not connected");
+                }
+            }
+        } catch (Exception e) {
+            Log.e(LOCATION_TAG, "" + e.toString());
+        }
+        printGPS();
+        return location;
+    }
+
+    public void stopUsingGPS() {
+        if (locationManager != null) {
+            locationManager.removeUpdates(BackService.this);
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
+    }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        printGPS();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+    @Override
+    public void onProviderDisabled(String s) {
     }
 }
