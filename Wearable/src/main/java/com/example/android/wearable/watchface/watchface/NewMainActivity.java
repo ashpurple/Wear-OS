c7734c9d7dd47a14ae986d0d869f0f08248a74c9
ashpurple <ashpurple9@gmail.com>
2021-12-01 19:46:39 +0900

    Update: DUID

--- Wearable/src/main/java/com/example/android/wearable/watchface/watchface/NewMainActivity.java
+++ Wearable/src/main/java/com/example/android/wearable/watchface/watchface/NewMainActivity.java
@@ -49,8 +49,8 @@ public class NewMainActivity extends Activity {
     private final static String[] permissions = new String[]
             {Manifest.permission.BODY_SENSORS, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
     private static final int REQUEST_RECORD_PERMISSION = 100;
-    static final String DUID = "MjA6MjA6MDg6Mjc6MDU6MDA"; // user's input
-    static final String MAC = "20:20:08:27:05:00"; // user's input
+    static final String DUID = "MDg6OTc6OTg6MEU6RTY6REE"; // user's input
+    static final String MAC = "08:97:98:0E:E6:DA"; // user's input
     /* Info Text */
     String jsonInput = "";
     TextView userText;
